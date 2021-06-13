ba763e2b92dc687bdb78a0eb227f817f3d3f27cf
kteska <kasia@teska.pl>
2021-06-13 19:52:56 +0200

    Added pagination

--- src/App.js
+++ src/App.js
@@ -1,39 +1,55 @@
 //import logo from './logo.svg';
-import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
-//import Home from './Home';
 import PokemonList from './PokemonList';
+import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  appBar: {
+    backgroundColor: 'red'
+  }
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          </IconButton>
+          <Typography variant="h6" className='title__bar'>
+            Pokedex
+          </Typography>
+          <Button color="inherit">Filter</Button>
+        </Toolbar>
+      </AppBar>
       <section className="App">
-    <Router>
-      {/* <Link to="/">Home</Link> */}
-      {/* <Link to="/search">Search</Link> */}
-      <Route path="/" component={PokemonList} />
-      {/* <Route path="/search" component={Search} /> */}
-    </Router>
-  </section>
+        <Router>
+          {/* <Link to="/">Home</Link> */}
+          {/* <Link to="/search">Search</Link> */}
+          <Route path="/" component={PokemonList} />
+          {/* <Route path="/search" component={Search} /> */}
+        </Router>
+      </section>
     </div>
   );
 }
--- src/PokemonList.js
+++ src/PokemonList.js
@@ -15,79 +15,15 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Pagination from '@material-ui/lab/Pagination';
 import { makeStyles } from '@material-ui/core/styles';
 
-
-const PokemonCard = ({ id, name, key }) => {
-    const classes = useStyles();
-    const [pokemon, setPokemon] = useState([]);
-    const [dialog, setDialog] = useState(false);
-
-    const getPokemonInfo = async (id) => {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-        const data = await response.json();
-        console.log(data);
-        setPokemon(data);
-    }
-
-    const handleSelectClick = (event) => {
-        console.log(event.currentTarget.id)
-        getPokemonInfo(event.currentTarget.id)
-        setDialog(true);
-    }
-
-    const handleClose = () => {
-        setDialog(false);
-    }
-
-    return (
-        <div>
-            <Card className={classes.card} key={key}>
-                <CardMedia
-                    className={classes.cardMedia}
-                    image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
-                />
-                <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        {name}
-                    </Typography>
-                    <Typography>
-                        {id}
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button size="small" color="primary" id={id} onClick={handleSelectClick}>
-                        View
-                    </Button>
-                </CardActions>
-            </Card>
-            <Dialog
-                open={dialog}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        Height: {pokemon.height}
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="primary">
-                        Disagree
-                    </Button>
-                    <Button onClick={handleClose} color="primary" autoFocus>
-                        Agree
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </div>
-    );
-}
-
-
 const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            marginTop: theme.spacing(4),
+        },
+    },
     icon: {
         marginRight: theme.spacing(2),
     },
@@ -105,10 +41,31 @@ const useStyles = makeStyles((theme) => ({
     card: {
         height: '100%',
         display: 'flex',
+        width: '300px',
         flexDirection: 'column',
+        paddingTop: '25px',
+        cursor: 'pointer'
     },
     cardMedia: {
         paddingTop: '56.25%', // 16:9
+        width: 170,
+        height: 0,
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    },
+    avatar: {
+        paddingTop: '56.25%', // 16:9
+        width: 190,
+        height: 0,
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        borderRadius: '100px',
+        marginTop: '10px'
+    },
+    dialogTitle: {
+        textAlign: 'center'
     },
     cardContent: {
         flexGrow: 1,
@@ -117,16 +74,100 @@ const useStyles = makeStyles((theme) => ({
         backgroundColor: theme.palette.background.paper,
         padding: theme.spacing(6),
     },
+    pagination: {
+        marginTop: '40px',
+        justifyContent: "center",
+        display: 'flex',
+        backgroundColor: 'white',
+        padding: '12px',
+        width: '102%'
+    }
 }));
+
+const PokemonCard = ({ id, name, key }) => {
+    const classes = useStyles();
+    const [pokemon, setPokemon] = useState([]);
+    const [dialog, setDialog] = useState(false);
+
+    const getPokemonInfo = async (id) => {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        const data = await response.json();
+        console.log(data.types[0].type.name)
+        setPokemon(data);
+        console.log(data)
+    }
+
+    const handleSelectClick = (event) => {
+        getPokemonInfo(event.currentTarget.id)
+        setDialog(true);
+    }
+
+    const handleClose = () => {
+        setDialog(false);
+    }
+
+    return (
+        <div>
+            <Card className={classes.card} key={key} id={id} onClick={handleSelectClick}>
+                <CardMedia
+                    className={classes.cardMedia}
+                    image={`https://pokeres.bastionbot.org/images/pokemon/${id}.png`}
+                />
+                <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {name}
+                    </Typography>
+                </CardContent>
+            </Card>
+            <Dialog
+                open={dialog}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+                fullWidth
+                maxWidth='xs'
+            >
+                <CardMedia
+                    className={classes.avatar}
+                    image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+                />
+                <DialogTitle id="alert-dialog-title" className={classes.dialogTitle}>
+                    {name}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description" className={classes.dialogTitle}>
+                        <Typography>Base experience: {pokemon.base_experience}</Typography>
+                        <Typography>Weight: {pokemon.weight}</Typography>
+                        <Typography> Height: {pokemon.height}</Typography>
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
+
 
 function PokemonList() {
     const classes = useStyles();
     const [pokemons, setPokemons] = useState([]);
+    const [page, setPage] = useState(1);
+
+    const handlePageChange = (event, value) => {
+        setPage(value);
+        console.log('val', value);
+    }
+
     const getPokemons = async () => {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=20&limit=9`)
         const data = await response.json();
         data.results.forEach(element => {
             element.id = element.url.split("/")[6];
+            element.name = element.name.toUpperCase();
         });
         setPokemons(data.results);
     }
@@ -138,13 +179,20 @@ function PokemonList() {
     return (
         <div className="App">
             <Container className={classes.cardGrid} maxWidth="md">
-                <Grid container spacing={4}>
+                <Grid container spacing={8}>
                     {pokemons.map((pokemon) => (
                         <Grid item key={pokemon.key} xs={12} sm={6} md={4}>
                             <PokemonCard key={pokemon.key} id={pokemon.id} name={pokemon.name} />
                         </Grid>
                     ))}
                 </Grid>
+                <Pagination
+                    count={10}
+                    color="secondary"
+                    className={classes.pagination}
+                    page={page}
+                    onChange={handlePageChange}
+                />
             </Container>
         </div>
     );
